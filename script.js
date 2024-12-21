f3abca685a5aeb64733db35b4f15d5f23cd28f61
unknown <Gopaydota@gmail.com>
2024-12-21 07:08:58 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('scroll', function() {
+	const header = document.getElementById('header');
+	if (window.scrollY > 50) {
+			header.classList.add('header-scrolled');
+	} else {
+			header.classList.remove('header-scrolled');
+	}
+});
+
